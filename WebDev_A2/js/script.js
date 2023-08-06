bc1502b82200449212c9eb791a645df996698505
Eatennn <103941883+Eatennn@users.noreply.github.com>
2023-08-06 17:08:34 +0800

    Update script.js

--- WebDev_A2/js/script.js
+++ WebDev_A2/js/script.js
@@ -4,10 +4,39 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-function toggleHamsterInfo() {
-  var hamsterInfo = document.getElementById("hamsterInfo");
-  hamsterInfo.classList.toggle("show");
+
+
+// function toggleHamsterInfo() {
+//   var hamsterInfo = document.getElementById("hamsterInfo");
+//   hamsterInfo.classList.toggle("show");
+// }
+
+// function changeImage(imageSrc) {
+//   var hamsterImage = document.getElementById("hamsterImage");
+//   hamsterImage.src = imageSrc;
+// }
+
+// function changeSummary(summaryText) {
+//   var summaryElement = document.querySelector('details summary');
+//   summaryElement.textContent = summaryText;
+// }
+
+function changeContent(summaryText, imageSrc , hamsterDescription) {
+  //find <summary> within <details>
+  let summaryElement = document.querySelector('details summary');
+  //replace the text with the one defined when using the function
+  summaryElement.textContent = summaryText;
+
+   //find <img> with id "hamsterImage" 
+  let hamsterImage = document.getElementById('hamsterImage');
+  hamsterImage.src = imageSrc;
+
+   // Find <p> with id "hamsterDescription" and update its content
+   let description = document.getElementById('hamsterDescription');
+   description.textContent = hamsterDescription;
 }
+
+
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
