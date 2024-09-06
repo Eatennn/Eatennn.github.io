2af27b921e8f6f3ae496a04de1de2707d6074ad6
Eatennn <220195D@mymail.nyp.edu.sg>
2024-09-06 12:35:59 +0800

    goodluck

--- GpsChecker/js/app.js
+++ GpsChecker/js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/GpsChecker.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/5_9.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
